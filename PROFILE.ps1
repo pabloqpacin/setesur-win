34cad972e2bfcc5be62a4c5a0814e876fce62d74
pabloqpacin <110383181+pabloqpacin@users.noreply.github.com>
2024-02-14 18:21:06 +0100

    I'll rebase this

--- PROFILE.ps1
+++
@@ -1,5 +0,0 @@
-function hola { Write-Host "que pasa" }
-function wuup { winget update && winget upgrade --all }
-# Otras funciones, aliases, variables de entorno, modificaciones del $env:PATH, etc.
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\catppuccin.omp.json" | Invoke-Expression
-
